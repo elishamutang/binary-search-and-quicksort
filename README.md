9f21ae2ec98f759c218d08f91d843ba0143a316b
elishamutang <elishamutang@outlook.com>
2025-03-31 23:28:01 +0800

    Added README.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Summary
+My implementation of recursive binary search and quick sort algorithm
+in Python :)
+
+Just for learning purposes.
---
+++ quick_sort.py
@@ -0,0 +1,51 @@
+import random
+
+n = 200
+test_arr = random.sample(range(1, 201), n)
+print(f'Unsorted array: {test_arr}')
+
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr.pop(0)
+    smaller = []
+    larger = []
+
+    for value in arr:
+        if value < pivot:
+            smaller.append(value)
+        else:
+            larger.append(value)
+
+    return quicksort(smaller) + [pivot] + quicksort(larger)
+
+
+def binary_search(arr, num, searches=0):
+    searches += 1
+    mid_idx = len(arr) // 2
+    print(mid_idx, arr[mid_idx])
+
+    if arr[mid_idx] == num:
+        return f'Found {num} in {searches} searches!'
+
+    if arr[mid_idx] < num:
+
+        lower_bound = mid_idx + 1
+        upper_bound = len(arr)
+
+        return binary_search(arr[lower_bound:upper_bound], num, searches)
+
+    if arr[mid_idx] > num:
+
+        lower_bound = 0
+        upper_bound = mid_idx
+        return binary_search(arr[lower_bound:upper_bound], num, searches)
+
+
+sorted_arr = quicksort(test_arr)
+print(f'Sorted array: {sorted_arr}')
+
+print(binary_search(sorted_arr, 20))
+
